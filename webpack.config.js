35c91c3480df462227027d822823db37c5b1591a
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-11 15:16:45 -0800

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+var webpack = require("webpack");
 var path = require("path");
 var SRC_DIR = path.join(__dirname, "/client");
 var DIST_DIR = path.join(__dirname, "/static");
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
+  mode: "development",
   output: {
     filename: "bundle.js",
     path: DIST_DIR
@@ -15,7 +17,7 @@ module.exports = {
         include: SRC_DIR,
         loader: "babel-loader",
         query: {
-          presets: ["react", "es2015"]
+          presets: ["@babel/preset-env", "@babel/preset-react"]
         }
       }
     ]
