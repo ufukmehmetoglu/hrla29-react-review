6b47397f9bd928d1fc6a7a6ff6fd920f16ec2fc5
kyijulie <julieim@Julies-MacBook-Air.local>
2019-01-11 14:42:47 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const parser = require("body-parser");
+const path = require("path");
+const PORT = 4000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../static")));
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log("App is listening on PORT:", PORT));
