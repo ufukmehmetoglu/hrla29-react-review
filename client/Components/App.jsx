59c817dc7185d1b7ea1d513a3bc2ae61b9f90d86
Jennifer Haren <jennifer.haren110@gmail.com>
2019-04-23 09:15:33 -0700

    Exercise 6 complete

--- client/Components/App.jsx
+++ client/Components/App.jsx
@@ -7,20 +7,28 @@ class App extends React.Component {
       firstName: ''
     };
     this.enterFirst = this.enterFirst.bind(this);
+    this.enterLast = this.enterLast.bind(this);
   }
 
   enterFirst(event) {
-    console.log('THIS:::::::', this);
     this.setState({
       firstName: event.target.value
-    })
+    });
+  }
+
+  enterLast(event) {
+    this.setState({
+      lastName: event.target.value
+    });
   }
   
   render() {
     return (
       <div>
         {this.state.firstName} <br /> 
-        First Name: <input name="first" onChange={this.enterFirst} /> Last Name: {this.props.lastName}
+        First Name: <input name="first" onChange={this.enterFirst} /><br />
+        {this.state.lastName} <br /> 
+        Last Name: <input name="last" onChange={this.enterLast} />
       </div>
     );
   }
